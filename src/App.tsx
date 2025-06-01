8a1c176eb6c6628f487fd955493974e88077ea3e
rwawine <rubchenya.work@gmail.com>
2025-06-01 13:35:55 +0300

    Refactor Vercel and Vite configurations
    - Updated routing syntax in vercel.json from "src" and "dest" to "source" and "destination" for consistency.
    - Changed routing from HashRouter to BrowserRouter in App.tsx for improved navigation.
    - Added strict port setting in vite.config.ts to enforce server port usage.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { HelmetProvider } from 'react-helmet-async';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import styles from './App.module.css';
 import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
